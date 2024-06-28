9582d2cd6cd40eace07460a44514bc1c40ab0427
devingk <devcharmingk@naver.com>
2024-06-28 23:00:40 +0900

    게시판 글 삭제 API requestBody에 validation 어노테이션 추가

--- src/main/kotlin/me/mk/hello_kopring/dto/ArticleUpdateRequest.kt
+++ src/main/kotlin/me/mk/hello_kopring/dto/ArticleUpdateRequest.kt
@@ -1,6 +1,13 @@
 package me.mk.hello_kopring.dto
 
+import jakarta.validation.constraints.NotBlank
+import me.mk.hello_kopring.constant.ErrorMessages.Companion.NOT_BLANK
+
 data class ArticleUpdateRequest(
+
+    @field:NotBlank(message = "The title $NOT_BLANK")
     val title: String,
+
+    @field:NotBlank(message = "The contents $NOT_BLANK")
     val contents: String
 )
